6fd2be8163087b4beb26c2d5248233c5f98598fb
Daniel Doughty <djdoughty@gmail.com>
2018-04-05 21:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector (selector) {
 }
 
 function nestedTarget() {
-return document.querySelector('#nested.target')
+return document.getElementById.querySelector('#nested.target')
 }
 nestedTarget()
 document.querySelector('div')
