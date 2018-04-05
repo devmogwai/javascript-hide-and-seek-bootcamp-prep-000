7c8400305a448b69d87a182b8723ab3e7e8c58a5
Daniel Doughty <djdoughty@gmail.com>
2018-04-05 21:37:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ function getFirstSelector (selector) {
   return document.querySelector(selector)
 }
 
-// function nestedTarget() {
-// return document.querySelector(nested)
-// }
-// nestedTarget()
-document.querySelector('div')
+function nestedTarget() {
+return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var lis=document.querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<lis.length;i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
+}
